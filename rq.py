570435684d4a5b6d341eb3bbfaa275f0d434d03a
sander <s.teunissen@gmail.com>
2015-06-18 10:17:03 +0200

    going for it

--- rq.py
+++ rq.py
@@ -1,22 +1,29 @@
 import json
+import time
+import logging
+
+lgr=logging.getLogger(__name__)
 
 __author__ = 'sander'
 
 class rqSet():
     def __init__(self):
         self.nodes=[]
+        self.log_list=TimedList()
 
     def parse(self,raw):
         address = self.get_address(raw)
         node =self.get_node(address)
         if node:
-            node.raw_feedback.append(self.get_rawfeedback(raw))
+            #node.raw_history.new_feedback(self.get_rawfeedback(raw))
+            node.log_feedback(self.get_rawfeedback(raw))
         else:
-            node = rqBaseNode(address)
+            node = rqBaseNode(self.log_list,address=address)
             self.nodes.append(node)
 
     def get_rawfeedback(self,raw):
         return raw[4:]
+
     def get_address(self,raw):
         return raw[1:4]
 
@@ -32,14 +39,38 @@ class rqSet():
         with open(file_path,'r') as fl:
             js = json.load(fl)
         for ndjs in js:
-            nd = rqBaseNode()
+            nd = rqBaseNode(self.log_list)
             for key,val in ndjs.items():
-                if type(val)==str:
+                if key == "raw_history" or key == "current_raw":
+                    pass
+                elif type(val)==str:
                     setattr(nd,key,bytes(val,'ascii'))
                 else:
                     setattr(nd,key,val)
             self.nodes.append(nd)
 
+class TimedList(list):
+    # def __init__(self,iterable=None):
+    #     list.__init__(self,iterable)
+
+    def new_uplink(self,uplink_command):
+        lgr.info("new uplink command: {}".format(uplink_command))
+        upl = ((time.time(),[uplink_command]))
+        self.append(upl)
+        return upl
+
+    # def new_feedback(self,feedback):
+    #     lgr.info("    new feedback : {}".format(feedback))
+    #     self[-1][1].append(feedback)
+    #
+    # def append(self, p_object):
+    #     self.append((time.time(),p_object))
+def uplink_decorator(func):
+    def wrapper(self,*args):
+        c = func(self,*args)
+        self.current_raw = self.raw_history.new_uplink(c)
+        return c
+    return wrapper
 
 class rqBaseNode():
     @staticmethod
@@ -49,31 +80,45 @@ class rqBaseNode():
         return obj.__dict__
 
     def report(self):
-        return ("{} {}".format(self.to_string(self.address),self.to_string(b' '.join(self.raw_feedback))))
+        return ("{} {}".format(self.to_string(self.address),self.to_string(b' '.join(self.raw_history))))
+
+    def log_feedback(self,raw):
+        self.current_raw[1].append(raw)
 
     def to_string(self,byte_array):
         return byte_array.decode(encoding='UTF-8')
 
-    def __init__(self,address=None):
+    def __init__(self,raw_list,serial=None,address=None):
         self.address=address
-        self.raw_feedback=[]
+        self.raw_history=raw_list
+        self.serial=serial
+        self.current_raw=[]
 
+    @uplink_decorator
     def get_version(self):
         c = bytearray(b'!')
         c.extend(self.address)
         c.extend(b"v?;")
         return c
 
-    def get_lift_and_tilt(self):
+    @uplink_decorator
+    def get_lift_and_tilt(self,lift,tilt):
         c=bytearray(b'!')
         c.extend(self.address)
-        c.extend(b"m50b30;")
+        c.extend(b"m")
+        c.extend(bytes(str(lift),encoding='ascii'))
+        c.extend(b"b")
+        c.extend(bytes(str(tilt),encoding='ascii'))
+        c.extend(b";")
         return c
 
-    def get_move(self):
+    @uplink_decorator
+    def get_move(self,move):
         c=bytearray(b'!')
         c.extend(self.address)
-        c.extend(b"m50;")
+        c.extend(b"m")
+        c.extend(bytes(str(move),encoding='ascii'))
+        c.extend(b";")
         return c
 
 
--- serial_test.py
+++ serial_test.py
@@ -5,91 +5,123 @@ from rq import rqSet
 __author__ = 'sander'
 import serial
 import threading
+import logging
+
+lgr =logging.getLogger(__name__)
 
 parser=argparse.ArgumentParser(description="serial tester")
 parser.add_argument("port",type=str)
 parser.add_argument("interval",type=float,help='send interval in seconds.')
 parser.add_argument('addresses',type=str)
 
+
+
 class Test():
+    end_comm = bytes(';','ascii')
     def __init__(self,port,interval,addresses):
         self.ser = serial.Serial(port=c_port,timeout=1)
+        self.alive=True
+        self.rq_set=rqSet()
+        self.rq_set.from_json(addresses)
 
-def read_serial(serial,alive,rq_set):
-    end_comm = bytes(';','ascii')
-    return_count=0
-    cmd = bytearray()
-    while alive:
-        #print ("alive?: {}".format(alive))
-        rq_in = serial.read(1)
-        if rq_in != b'':
-            if rq_in==end_comm:
-                return_count+=1
-                rq_set.parse(cmd)
-                #print ('rq command returned: {}. Command count: {}'.format(cmd,return_count))
-                cmd=bytearray()
+        self.t = threading.Thread(target=self.read_serial)
+        self.t.setDaemon(True)
+        self.t.start()
+
+        self.move=0
+        self.increase=10
+        for i in range(5):
+            if self.move==90:
+                self.increase=-10
+            elif self.move==10:
+                self.increase=10
+            self.move+=self.increase
+            for node in self.rq_set.nodes:
+                self.ser.write(node.get_move(self.move))
+            time.sleep(5)
+
+        # wait for all feedback to arrive.
+        time.sleep(30)
+        self.alive=False
+        for itm in self.rq_set.log_list:
+            st = ",".join((str(x,encoding='ascii').ljust(10) for x in itm[1]))
+            lgr.debug(st)
+        lgr.debug("closing connection")
+        self.ser.close()
+        self.t.join()
+
+
+    def read_serial(self):
+        cmd = bytearray()
+        while self.alive:
+            rq_in = self.ser.read(1)
+            if rq_in != b'':
+                if rq_in==Test.end_comm:
+                    self.rq_set.parse(cmd)
+                    cmd=bytearray()
+                else:
+                    cmd.extend(rq_in)
             else:
-                cmd.append(rq_in[0])
-        else:
-            print("serial timeout")
-    print("closing connection")
-    serial.close()
+                lgr.debug("serial time out")
+        lgr.debug("closing reading thread")
+
 
 
 
 
 if __name__=="__main__":
-    alive=True
-
+    #alive=True
+    logging.basicConfig(level=logging.DEBUG)
     args= parser.parse_args()
     c_port = args.port
     interval = args.interval
     addres_file = args.addresses
 
-    rq_set = rqSet()
-    rq_set.from_json(addres_file)
-    print("amount of nodes: {}".format(len(rq_set.nodes)))
-    commands_sent=0
+    tester=Test(c_port,interval,addres_file)
+    #rq_set = rqSet()
+    #rq_set.from_json(addres_file)
+    #print("amount of nodes: {}".format(len(rq_set.nodes)))
+    #commands_sent=0
     #ser = serial.Serial(args['port'],baudrate=115200)
-    ser = serial.Serial(port=c_port,timeout=1)
-    for node in rq_set.nodes:
-        time1=time.time()
-        #vers = node.get_version()
-        vers=node.get_lift_and_tilt()
-        #vers=node.get_move()
-        ser.write(vers)
-
-        if interval:
-            time.sleep(interval)
-        time2=time.time()
-
-        commands_sent+=1
-        print ("Command #{} sent to {} sent at timediff {}".format(commands_sent, node.address,time2-time1))
-    # t=threading.Thread(target=read_serial,args=(ser,alive,rq_set))
-    # t.setDaemon(True)
-    # t.start()
-    # # give it some time to fill the serial port.
-    end_comm = bytes(';','ascii')
-    return_count=0
-    cmd = bytearray()
-    while alive:
-        #print ("alive?: {}".format(alive))
-        try:
-            rq_in = ser.read(1)
-            if rq_in != b'':
-                if rq_in==end_comm:
-
-                    return_count+=1
-                    rq_set.parse(cmd)
-                    #print ('rq command returned: {}. Command count: {}'.format(cmd,return_count))
-                    cmd=bytearray()
-                else:
-                    cmd.append(rq_in[0])
-            else:
-                print("serial timeout")
-                #alive=False
-        except KeyboardInterrupt:
-            print('keyboard interupt')
-            alive=False
-    for node in rq_set.nodes:
-        print(node.report())
+    #ser = serial.Serial(port=c_port,timeout=1)
+    # for node in rq_set.nodes:
+    #     time1=time.time()
+    #     #vers = node.get_version()
+    #     vers=node.get_lift_and_tilt()
+    #     #vers=node.get_move()
+    #     ser.write(vers)
+    #
+    #     if interval:
+    #         time.sleep(interval)
+    #     time2=time.time()
+    #
+    #     commands_sent+=1
+    #     print ("Command #{} sent to {} sent at timediff {}".format(commands_sent, node.address,time2-time1))
+    # # t=threading.Thread(target=read_serial,args=(ser,alive,rq_set))
+    # # t.setDaemon(True)
+    # # t.start()
+    # # # give it some time to fill the serial port.
+    # end_comm = bytes(';','ascii')
+    # return_count=0
+    # cmd = bytearray()
+    # while alive:
+    #     #print ("alive?: {}".format(alive))
+    #     try:
+    #         rq_in = ser.read(1)
+    #         if rq_in != b'':
+    #             if rq_in==end_comm:
+    #
+    #                 return_count+=1
+    #                 rq_set.parse(cmd)
+    #                 #print ('rq command returned: {}. Command count: {}'.format(cmd,return_count))
+    #                 cmd=bytearray()
+    #             else:
+    #                 cmd.append(rq_in[0])
+    #         else:
+    #             print("serial timeout")
+    #             #alive=False
+    #     except KeyboardInterrupt:
+    #         print('keyboard interupt')
+    #         alive=False
+    # for node in rq_set.nodes:
+    #     print(node.report())
